54374037dfeedd2584e8b16cf2a62f2b9acacac4
Jyoti Yadav <jyotiyadavofficial2022@gmail.com>
2023-10-21 18:35:25 +0530

    make signup

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -2,11 +2,23 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/styles";
+import { RxAvatar } from "react-icons/rx";
+
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(false);
+  const [avatar, setAvatar] = useState(null);
+
+  const handleSubmit = () => {
+    console.log("fff");
+  };
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    setAvatar(file);
+  };
 
   return (
     <div className="min-h-screen bg-blue-200 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -94,6 +106,35 @@ const Signup = () => {
                 htmlFor="avatar"
                 className="block text-sm font-medium text-gray-700"
               ></label>
+              <label>
+                <div className="mt-2 flex items-center">
+                  <span className="inline-block h-8 w-8 rounded-full overflow-hidden">
+                    {avatar ? (
+                      <img
+                        src={URL.createObjectURL(avatar)}
+                        alt="avatar"
+                        className="h-full w-full object-cover rounded-full"
+                      />
+                    ) : (
+                      <RxAvatar className="h-8 w-8" />
+                    )}
+                  </span>
+                  <label
+                    htmlFor="file-input"
+                    className="ml-5 flex items-centr justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
+                  >
+                    <span>Upload a file</span>
+                    <input
+                      type="file"
+                      name="avatar"
+                      id="file-input"
+                      accept=".jpg,.jpeg,.png"
+                      onChange={handleFileInputChange}
+                      className="sr-only"
+                    ></input>
+                  </label>
+                </div>
+              </label>
             </div>
 
             <div>
@@ -105,9 +146,9 @@ const Signup = () => {
               </button>
             </div>
             <div className={`${styles.normalFlex} w-full`}>
-              <h4> Have any account?</h4>
+              <h4> Already Have any account?</h4>
               <Link to="/login" className="text-blue-700 font-medium pl-2">
-                Login
+                Sign In
               </Link>
             </div>
           </form>
